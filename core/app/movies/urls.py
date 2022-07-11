0b892a9e1a98cf385386e55018b9647f4f2b0336
Алексей <sinsobaki666@gmail.com>
2022-07-11 07:47:54 +0300

    почините шапку на главном сайте!!!

--- core/app/movies/urls.py
+++ core/app/movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import index_page,info_page
+from .views import index_page, info_page, player
 
 urlpatterns = [
     path('', index_page),
     path('about-us', info_page),
+    path('movies/<str:name>', player)
 ]
 
--- core/app/movies/views.py
+++ core/app/movies/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from .models import Movie
 
+
 def index_page(request):
     movies = Movie.objects.all()
-    return render(request, 'index.html', {'movies': movies})
+    return render(request, 'movie_list.html', {'movies': movies})
 # Create your views here.
+
+
 def info_page(request):
-    return render(request,'info.html')
+    return render(request, 'info.html')
+
+
+def player(request, name):
+    magnet_link = filter(lambda x: str(x.name).startswith(name), list(Movie.objects.all()))
+    magnet_link = list(magnet_link).pop().magnet_link
+    return render(request, 'player.html', {'magnet_link': magnet_link})
